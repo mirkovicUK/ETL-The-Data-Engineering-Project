12b339e782b6da1468d6f72e254924beaec1fd70
mirkovic.uk <mirkovic.uk@gmail.com>
2024-02-13 18:36:21 +0000

    asdasd

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "lambda_parquet" {
   function_name = "${var.parquet_lambda_name}"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "json_to_parquet.json_to_parquet"
-  runtime = "python3.11"
+  runtime = "python3.10"
   timeout          = 180
   layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python311:5"]
 }
@@ -25,7 +25,7 @@ resource "aws_lambda_function" "lambda_josn_to_parquet" {
   function_name = "${var.json_to_parquet_lambda}"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "parquet_to_json.parquet_to_json"
-  runtime = "python3.11"
+  runtime = "python3.10"
   timeout          = 180
   layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python311:5"]
 }
@@ -45,7 +45,7 @@ resource "aws_lambda_function" "dummy_lambda" {
   function_name = "${var.dummy_json_lambda}"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "create_json.dummy"
-  runtime = "python3.11"
+  runtime = "python3.10"
   timeout          = 180
   layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python311:5"]
 }
