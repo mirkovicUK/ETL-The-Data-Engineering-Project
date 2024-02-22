bc619d6fcc776fde31659c58365f311aafb30e1c
mirkovic.uk <mirkovic.uk@gmail.com>
2024-02-22 14:35:04 +0000

    fix test bug

--- src/parquet_to_json.py
+++ src/parquet_to_json.py
@@ -41,6 +41,8 @@ def parquet_to_json(event, context):
         last_update = dt.now()
         #writing data
         write_dim_staff(con, json['data'][2]['dim_staff'], last_update)
+        write_dim_counterparty(con, json['data'][3]['dim_counterparty'], last_update)
+        
         
 
         #write to db sales
@@ -136,3 +138,41 @@ def get_secret(secret_name):
 
     secret = get_secret_value_response['SecretString']
     return  json.loads(secret)
+
+
+def write_dim_counterparty(con, data, updated=dt.now()):
+    dim_counterparty_column = ['counterparty_record_id', 
+    'counterparty_id', 'counterparty_legal_name',
+    'counterparty_legal_address_line_1', 'counterparty_legal_address_line_2',
+    'counterparty_legal_district', 'counterparty_legal_city', 'counterparty_legal_postal_code',
+    'counterparty_legal_country', 'counterparty_legal_phone_number', 'last_updated_date',
+    'last_updated_time']
+
+    for data_point in data:
+        values = [
+            data_point['counterparty_id'],
+            data_point['counterparty_id'],
+            data_point['counterparty_legal_name'],
+            data_point['counterparty_legal_address_line_1'],
+            data_point['counterparty_legal_address_line_2'],
+            data_point['counterparty_legal_district'],
+            data_point['counterparty_legal_city'],
+            data_point['counterparty_legal_postal_code'],
+            data_point['counterparty_legal_country'],
+            data_point['counterparty_legal_phone_number'],
+            updated.date(),
+            updated.time()
+        ]
+
+        dim_counterparty_query = f"""
+        INSERT INTO dim_counterparty
+        VALUES
+        ({literal(values[0])},{literal(values[1])},
+        {literal(values[2])},{literal(values[3])},
+        {literal(values[4])},{literal(values[5])},
+        {literal(values[6])},{literal(values[7])},
+        {literal(values[8])},{literal(values[9])},
+        {literal(values[10])},{literal(values[11])})
+        ON CONFLICT DO NOTHING;
+        """ 
+        con.run(dim_counterparty_query)
