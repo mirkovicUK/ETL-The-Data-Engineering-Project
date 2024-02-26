2cca6c6f8132a0e2e501616a07db391451a121be
A-Waterhouse <awaterhouse.mail@gmail.com>
2024-02-26 17:55:27 +0000

    added tests and checks to deployment

--- tests/test_write_dim_staff.py
+++ tests/test_write_dim_staff.py
@@ -1,13 +1,13 @@
-from src.utils.writing_utils.write_dim_staff import write_dim_staff as wds 
-from src.utils.writing_utils.get_secret import get_secret 
+from pg8000.native import Connection, literal, identifier, DatabaseError
+from src.utils.writing_utils.write_dim_staff import write_dim_staff as wds
+from src.utils.writing_utils.get_secret import get_secret
 from datetime import datetime as dt
 import pytest
 
 from awswrangler import _utils
 pg8000 = _utils.import_optional_dependency("pg8000")
-from pg8000.native import Connection, literal, identifier, DatabaseError
 
-
+@pytest.mark.skip
 @pytest.mark.describe('write_dim_staff()')
 @pytest.mark.it('correct_data_is_written_to_DB')
 def test_correct_data_is_written_to_DB():
@@ -18,12 +18,12 @@ def test_correct_data_is_written_to_DB():
         {'staff_record_id': 4, 'first_name': 'Ana', 'last_name': 'Glover', 'department_name': 'Production', 'location': 'Leeds', 'email_address': 'ana.glover@terrifictotes.com'},
         {'staff_record_id': 5, 'first_name': 'Magdalena', 'last_name': 'Zieme', 'department_name': 'HR', 'location': 'Leeds', 'email_address': 'magdalena.zieme@terrifictotes.com'}
     ]
-    # secret = get_secret('DB_write')
-    # con = Connection(secret['username'], 
-    #                 host = secret['host'],
-    #                 database = secret['dbname'],
-    #                 password = secret['password'])
-    #wds(con, data, dt.now() )
-    #rows = con.run("DELETE FROM dim_staff; ;")
-    # rows = con.run("SELECT * FROM dim_staff ;")
-    # print(*rows, '<----------DIM_STAFF', sep='\n')
+    secret = get_secret('new_tote')
+    con = Connection(secret['username'],
+                    host = secret['host'],
+                    database = secret['dbname'],
+                    password = secret['password'])
+    wds(con, data, dt.now() )
+    rows = con.run("DELETE FROM dim_staff; ;")
+    rows = con.run("SELECT * FROM dim_staff ;")
+    print(*rows, '<----------DIM_STAFF', sep='\n')
