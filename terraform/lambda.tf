1765557e40862b0b791f55d9ca86150de8c07e68
A-Waterhouse <awaterhouse.mail@gmail.com>
2024-02-27 11:18:46 +0000

    ran 1st deploy to fix bucket error

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,16 +10,6 @@ resource "aws_lambda_function" "lambda_parquet" {
   timeout          = 180
   layers           = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310:8"]
 
-  filename      = "${path.module}/../json_to_parquet.zip"
-  function_name = "${var.parquet_lambda_name}"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "json_to_parquet.json_to_parquet"
-  runtime = "python3.10"
-  timeout          = 900
-  layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310:8"]
-
-  source_code_hash = filebase64sha256("../json_to_parquet.zip")
-  memory_size = 512
 }
 
 # lambda triger  
@@ -44,16 +34,6 @@ resource "aws_lambda_function" "lambda_josn_to_parquet" {
   timeout          = 180
   layers           = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310:8"]
 
-  filename      = "${path.module}/../parquet_to_json.zip"
-  function_name = "${var.json_to_parquet_lambda}"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "parquet_to_json.parquet_to_json"
-  runtime = "python3.10"
-  timeout          = 900
-  layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310:8"]
-
-  source_code_hash = filebase64sha256("../parquet_to_json.zip")
-  memory_size = 512
 }
 resource "aws_lambda_permission" "allow_s3_parquet" {
   statement_id   = "Allow_s3"
@@ -77,16 +57,6 @@ resource "aws_lambda_function" "dummy_lambda" {
   timeout          = 180
   layers           = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310:8"]
 
-  filename      = "${path.module}/../ingestion.zip"
-  function_name = "${var.dummy_json_lambda}"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "ingestion.ingestion"
-  runtime = "python3.10"
-  timeout          = 900
-  layers = ["arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310:8"]
-
-  source_code_hash = filebase64sha256("../ingestion.zip")
-  memory_size = 512
 }
 
 resource "aws_cloudwatch_event_rule" "scheduler" {
