0143e1fab0513639b832aa05c6d6580c4805523a
A-Waterhouse <awaterhouse.mail@gmail.com>
2024-02-27 11:39:53 +0000

    added new error for string runtime

--- src/parquet_to_json.py
+++ src/parquet_to_json.py
@@ -49,10 +49,6 @@ s3_procesed_zone_url = read_processed_bucket_name()
 
 DB = 'data_warehouse'
 
-s3_procesed_zone_url = 's3://processed-zone-895623xx35/'
-DB = 'DB_write'
-
-
 
 def parquet_to_json(event, context):
     try:
